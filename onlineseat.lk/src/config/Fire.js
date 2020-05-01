fd49d2688965011335a80fc295d3d0a9ac02d39f
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-01 11:06:39 +0530

    added firebase to the app

---
+++ onlineseat.lk/src/config/Fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqGXeGO14a-ZkWTeG7Wr-pASEmLLJik30",
+    authDomain: "bus-booking-43159.firebaseapp.com",
+    databaseURL: "https://bus-booking-43159.firebaseio.com",
+    projectId: "bus-booking-43159",
+    storageBucket: "bus-booking-43159.appspot.com",
+    messagingSenderId: "803354273782",
+    appId: "1:803354273782:web:6f179ba9defd578ccec9d7",
+    measurementId: "G-M8SVVCXG4F"
+  };
+  const fire = firebase.initializeApp(config);
+  export default fire;
